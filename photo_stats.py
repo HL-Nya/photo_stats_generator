09c2107a3e23a7686a04e41dbc9153494a4e7316
Edward Du <edward.drj@gmail.com>
2021-10-04 19:13:29 -0400

    add sorting for output

--- photo_stats.py
+++ photo_stats.py
@@ -6,7 +6,6 @@ def photo_stats(root_dir, file_formats):
 	count = 0
 	result_dict = {
 					"EXIF LensModel":{}, 
-					"EXIF FocalLengthIn35mmFilm": {}, 
 					"Image Model": {},
 					"Image Make": {},
 					"Image Software": {},
@@ -24,12 +23,16 @@ def photo_stats(root_dir, file_formats):
 				f = open(f'{root}\\{file}', 'rb')
 				tags = exifread.process_file(f)
 				for tag in tags.keys():
+
 					if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
+						print(tag)
+						print(tags[tag])
 						if tag in result_dict.keys():
+
 							if str(tags[tag]) in result_dict[tag].keys():
 								result_dict[tag][str(tags[tag])] += 1
 							else:
-								if tag in ["EXIF ISOSpeedRatings", "EXIF FocalLengthIn35mmFilm"]:
+								if tag in ["EXIF ISOSpeedRatings"]:
 									result_dict[tag][int(str(tags[tag]))] = 1
 								else:
 									result_dict[tag][str(tags[tag])] = 1
@@ -41,10 +44,11 @@ def photo_stats(root_dir, file_formats):
 	for tag in result_dict.keys():
 		print(f"Stats for {tag}")
 		total = 0
-		for key in sorted(result_dict[tag]):
+
+		for key in result_dict[tag]:
 			total += result_dict[tag][key]
-		for key in sorted(result_dict[tag]):
-			print(f"\t{key}\n\t\t\tcount: {result_dict[tag][key]}\tPercentage: {str(round(result_dict[tag][key]/total*100,2))}%")
+		for key in sorted(result_dict[tag].items(), key=lambda x: x[1], reverse=True):
+			print(f"\t{key[0]}\n\t\t\tcount: {result_dict[tag][key[0]]}\tPercentage: {str(round(result_dict[tag][key[0]]/total*100,2))}%")
 
 
 
